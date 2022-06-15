5d108f4e16765c7354428f69cddea046f1388bbe
blubberalls <bluballsman@gmail.com>
2022-06-15 01:01:21 -0700

    Any jingle can be played now, forwarded points to points.check

--- commands/jingle.js
+++ commands/jingle.js
@@ -2,17 +2,25 @@ const jingleURL = 'https://storage.googleapis.com/discord_audio_jingles/MC%20Bal
 
 async function execute(interaction, args) {
     const voiceChannel = interaction.member.voice.channel;
+    const url = args['url'];
 
     if (voiceChannel == null || voiceChannel == undefined) {
         await interaction.reply({ content: 'You are not connected to a voice channel.' , ephemeral: true });
     }
     else {
-        await interaction.client.jingleHandler.playJingle(voiceChannel, jingleURL);
+        await interaction.client.jingleHandler.playJingle(voiceChannel, url);
         await interaction.reply({ content: 'Done!', ephemeral: true });
     }
 }
 
 module.exports = {
-    description: 'Plays the Ballyhoo jingle to the user connected in a voice chat.',
+    description: 'Plays a jingle to your currently connected voice chat.',
+    args: {
+        url: {
+            type: 'string',
+            description: 'URL directing to the .mp3 file to be played.',
+            optional: false
+        }
+    },
     execute: execute
 }
--- commands/points/.js
+++ commands/points/.js
@@ -1,3 +1,6 @@
+const check = require('./check.js');
+
 module.exports = {
-    description: 'Commands related to the points system on the Discord.'
+    description: 'Commands related to the points system on the Discord.',
+    execute: check.execute,
 }
