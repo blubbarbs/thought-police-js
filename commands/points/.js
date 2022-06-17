bd6a89d61b34e6e89ab03e2bc5f94010e61f1c09
blubberalls <bluballsman@gmail.com>
2022-06-17 03:37:49 -0700

    default points command checks your points

--- commands/points/.js
+++ commands/points/.js
@@ -1,19 +1,10 @@
-async function execute(interaction) {
-    const target = interaction.member;
-    let points = await client.userDataHandler.get(target.id, 'points');
-    points = points == null ? 0 : +points;
+const checkCommand = require('./check.js');
 
-    if (points == 0) {
-        await interaction.reply({ content: 'You have no points.', ephemeral: true });
-    }
-    else if (points == 1) {
-        await interaction.reply({ content: `You have 1 point.`, ephemeral: true });
-    }
-    else {
-        await interaction.reply({ content: `You have ${points} points.`, ephemeral: true });
-    }
+async function execute(interaction) {
+    await checkCommand.execute(interaction, { target: interaction.member });
 }
 
 module.exports = {
     description: 'Commands related to the points system on the Discord.',
+    execute: execute
 }
