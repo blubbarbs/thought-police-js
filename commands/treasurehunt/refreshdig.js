7b3b637507536ffcc87e6cd968b7e59d65ff79ce
blubberalls <bluballsman@gmail.com>
2022-07-07 03:28:28 -0700

    fixed command

--- commands/treasurehunt/refreshdig.js
+++ commands/treasurehunt/refreshdig.js
@@ -4,7 +4,7 @@ async function execute(interaction, args) {
     const target = args['target'];
     const treasureHunt = interaction.client.treasureHunt;
 
-    treasureHunt.setPlayerData(interaction.member.id, 'lastDigTime', null);
+    treasureHunt.setPlayerData(interaction.member.id, 'last_dig_time', null);
     await interaction.reply({ content: `Successfully refreshed the dig cycle for ${target}.`, ephemeral: true });
 }
 
